7d1d0926eaa96f8eb7f76cd151ce8b617514fcef
shams alam <103259015+shams220@users.noreply.github.com>
2025-10-24 09:59:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const select = querySelctor('#colorSelect');
+const btn = querySelector('input[type="button"]');
+btn.addEvenListener('click',()=>{
+	select.remove(select.selectedIndex)
+})
